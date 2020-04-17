a8207c4f4ba8188e94a28278f426783b86feb121
David Dulek <ddulek@gmail.com>
2020-04-17 15:33:27 -0500

    revert change

--- src/main/java/org/jboss/as/quickstarts/tasksrs/model/User.java
+++ src/main/java/org/jboss/as/quickstarts/tasksrs/model/User.java
@@ -28,9 +28,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import jakarta.xml.bind.annotation.XmlAttribute;
-import jakarta.xml.bind.annotation.XmlRootElement;
-import jakarta.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 
